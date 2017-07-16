2f7d81cd73f3642fdb8ebe7029476039b4cf91f1
Aleksandar Golubovic <golubaca@gmail.co>
2017-07-16 13:49:35 +0200

    Changed client to python3

--- client/recv.py
+++ client/recv.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python
-# TCP client example
-import socket,os, cv2
+import socket
+import cv2
 import numpy
 import random
 import sys
 
-host = 'localhost' if len(sys.argv) == 1 else sys.argv[1]
+host = sys.argv[1]
 cam_url = sys.argv[2]
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 client_socket.connect((host, 5005))
-k = ' '
-size = 1024
 name = str(random.random())
 
-client_socket.send(cam_url)
+client_socket.send(str.encode(cam_url))
 
 def rcv():
-    r = ''
-    data = ""
+    data = b''
     while 1:
+        
         try:
             r = client_socket.recv(90456)
             if len(r) == 0:
                 exit(0)
-            a = r.find('END!')
+            a = r.find(b'END!')
             if a != -1:
                 data += r[:a]
                 break
             data += r
         except Exception as e:
-            print e
+            print(e)
             continue
     nparr = numpy.fromstring(data, numpy.uint8)
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
